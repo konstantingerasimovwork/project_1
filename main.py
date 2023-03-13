1ed390f703569eba56b3b3b97172e0701928c3c4
Konstantin Gerasimov <konstgerasimovwork@gmail.com>
2023-03-13 16:55:17 +0300

    New lessons

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-result = 3**(4 + 0) ** 5
-print(result)
+weight = 75
+height = 175
+dist = 9.75
+hours = 2
+minutes = 2 * 60
+
+calories = (weight * 0.035 + (dist / hours) ** 2 / height * weight * 0.029) * minutes
+print(calories)
